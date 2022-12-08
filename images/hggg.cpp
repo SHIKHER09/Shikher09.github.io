065f14990f3ed33375d7a216f35c8371a7a4447d
Shikher Kumar <shikharrao1@gmail.com>
2022-12-08 18:57:17 +0530

    add img

---
+++ images/hggg.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+uisng namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int x=0;
+    for(int i=0;i<n;i++){
+        
+        string s;
+        cin>>s;
+        
+        if(s=="--x" || s=="--X"){
+            --x;
+        }
+        else if(s=="++x" || s=="++X"){ ++x;}
+    
+        else if(s=="x--" || s== "X--" ){x--;}
+        else { x++; }
+        
+    }
+    cout<<x;
+    return 0;
+}
