0baf11b21696585bb36790651a99dbf6fc20def9
Shikher Kumar <shikharrao1@gmail.com>
2023-05-22 11:42:29 +0530

    changes

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-console.log('Its working')
+console.log("Its working");
 
-let theme = localStorage.getItem('theme')
+let theme = localStorage.getItem("theme");
 
-if(theme == null){
-	setTheme('blue')
-}else{
-	setTheme(theme)
+if (theme == null) {
+  setTheme("blue");
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName("theme-dot");
 
-
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option clicked:", mode);
+    setTheme(mode);
+  });
 }
 
-function setTheme(mode){
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
+function setTheme(mode) {
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "default.css";
+  }
 
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
 
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
 
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
 
-	localStorage.setItem('theme', mode)
+  localStorage.setItem("theme", mode);
 }
